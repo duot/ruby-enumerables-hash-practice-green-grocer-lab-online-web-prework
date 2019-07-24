472ceec921340607e7a4a677d74915573738d14b
optimistic-writer-7178 <duot.jim@gmail.com>
2019-07-24 06:58:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -30,13 +30,15 @@ def apply_coupons(cart, coupons)
     if cart[item]
       #subtract discounted items from NON-discounted items
       #but only if coupon meets minimum amount
-      cart[item][:count] -= coupon[:num] if enough_items_for_coupon?(cart[item][:count], coupon[:num])
+      if enough_items_for_coupon?(cart[item][:count], coupon[:num])
+        cart[item][:count] -= coupon[:num] 
 
-      #add discounted items to cart
-      cart[item_w_coupon] = {count: 0} unless cart[item_w_coupon]
-      cart[item_w_coupon][:clearance] = cart[item][:clearance]
-      cart[item_w_coupon][:count] += coupon[:num] #if enough_items_for_coupon?(cart[item][:count], coupon[:num])
-      cart[item_w_coupon][:price] = price_of_each
+        #add discounted items to cart
+        cart[item_w_coupon] = {count: 0} unless cart[item_w_coupon]
+        cart[item_w_coupon][:clearance] = cart[item][:clearance]
+        # cart[item_w_coupon][:count] += coupon[:num] #if enough_items_for_coupon?(cart[item][:count], coupon[:num])
+        cart[item_w_coupon][:price] = price_of_each
+      end
     end
   end
   return cart
