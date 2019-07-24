5b7c1d1cd1aa5072e03a905ee0cec3f82e9060fd
optimistic-writer-7178 <duot.jim@gmail.com>
2019-07-24 06:22:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,7 +47,7 @@ end
 
 def checkout(cart, coupons)
   p all = apply_clearance(apply_coupons(consolidate_cart(cart), coupons))
-  p all.values.map { |item| 
+  p all.values.map { |item|
     price = item.fetch(:price)
     price *= item.fetch(:count)
   }.reduce(:+)
